5e62817a5b6e3a892a8a1d7ddd196b3938c9394c
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-24 18:59:04 -0300

    add script to send email

---
+++ scripts/send-email.sh
@@ -0,0 +1,4 @@
+ls
+sudo apt-get install mailutils
+echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" EMAIL_GABRIEL
+echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" EMAIL_DAVI
