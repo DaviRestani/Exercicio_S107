d3e5fa97fc906d5c433a6eef114e7c59a619d105
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-24 20:18:13 -0300

    teste sem echo

--- scripts/send-email.sh
+++ scripts/send-email.sh
@@ -2,5 +2,5 @@ ls
 echo "Inicio da instalação"
 sudo apt-get install mailutils
 echo "Fim da instalação"
-mail -s "subject:Exercicio_S207" $EMAIL_GABRIEL <<< 'Pipeline executada com sucesso!'
-mail -s "subject:Exercicio_S207" $EMAIL_DAVI <<< 'Pipeline executada com sucesso!'
+mail -s "subject:Exercicio_S207" "$EMAIL_GABRIEL" <<< 'Pipeline executada com sucesso!'
+mail -s "subject:Exercicio_S207" "$EMAIL_DAVI" <<< 'Pipeline executada com sucesso!'
