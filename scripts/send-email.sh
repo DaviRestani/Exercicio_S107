5f33e5f0119c0782a3760fecd7bfee6583d01d4b
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-24 19:17:21 -0300

    add email test to test

--- scripts/send-email.sh
+++ scripts/send-email.sh
@@ -1,4 +1,5 @@
 ls
 sudo apt-get install mailutils
-echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" EMAIL_GABRIEL
-echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" EMAIL_DAVI
+echo $EMAIL_TEST
+echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" "$EMAIL_GABRIEL"
+echo "Pipeline executada com sucesso!" | mail -s "subject:Pipeline Exercicio_S207" "$EMAIL_DAVI"
