fbc8c29a06b5c04d614db1b87a917bdda95a316f
GabrielHS <gabrielhenri0012@gmail.com>
2022-09-25 19:20:19 -0300

    add script sh

--- scripts/send-email.sh
+++ scripts/send-email.sh
@@ -2,4 +2,4 @@ echo "Inicio da instalação"
 sudo apt-get install mailutils
 echo "Fim da instalação"
 echo "Build executada com sucesso!" | mail -s "subject: qualquer coisa" "$EMAIL_DAVI"
-echo "Build executada com sucesso!" | mail -s "subject: qualquer coisa" $EMAIL_GABRIEL
+echo "Build executada com sucesso!" | mail -s "subject: qualquer coisa" "$EMAIL_GABRIEL"
